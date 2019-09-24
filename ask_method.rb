427ec811b74e311120ae2df3ae93f2b919a9af36
yerlanmad <43934798+yerlanmad@users.noreply.github.com>
2019-09-24 21:38:32 +0600

    Add files via upload

--- ask_method.rb
+++ ask_method.rb
@@ -1,35 +1,32 @@
 ﻿# Метод для расчета кол-ва секунд до Нового года
 def remain_sec(time_now)
-	return Time.new(time_now.year + 1).to_i - time_now.to_i
+  Time.new(time_now.year + 1).to_i - time_now.to_i
 end
 
 # Метод для вывода информации по запросу
 def ask(str)
 
-	err_msg = "Был передан не корректный запрос"
+  # Проверка, является ли переданный аргумент строкой
+  return unless str.is_a?(String)
+
+  err_msg = "Был передан не корректный запрос"
   time = Time.new
 
-  # Проверка, является ли переданный аргумент строкой
-  if str.is_a? String
-	  case str
-	  when "time" 
-	  	puts time.strftime("Текущее время: %H:%M")
-	  when "date" 
-	  	puts time.strftime("Текущая дата: %d %B, %Y")
-	  when "day" 
-	  	puts time.strftime("Текущий день недели: %A")
-	  when "remaining days" 
-	  	puts "До нового года осталось #{remain_sec(time) / 86400} дней"
-	  when "remaining weeks" 
-	  	puts "До нового года осталось #{remain_sec(time) / 86400 / 7} недель"
-	  else 
-	  	puts err_msg
-	  end
-	else
-		puts err_msg
-	end
+  case str
+  when "time" 
+    puts time.strftime("Текущее время: %H:%M")
+  when "date" 
+    puts time.strftime("Текущая дата: %d %B, %Y")
+  when "day" 
+    puts time.strftime("Текущий день недели: %A")
+  when "remaining days" 
+    puts "До нового года осталось #{remain_sec(time) / 86400} дней"
+  when "remaining weeks" 
+    puts "До нового года осталось #{remain_sec(time) / 86400 / 7} недель"
+  else 
+    puts err_msg
+  end
 end
-
 
 print "Введите ваш запрос: "
 request = gets.chomp
